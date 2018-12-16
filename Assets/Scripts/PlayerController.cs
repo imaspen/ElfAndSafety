de9b731a34f73d88f15501dc5fee14d37da43fb0
Aspen <zebington@gmail.com>
2018-12-16 15:07:18 +0000

    implemented basic snowman

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private float _nextFire = 0.0f;
 
-    [SerializeField] private float fireDelay = 0.5f;
-    [SerializeField] private int maxSpeed;
-    [SerializeField] private GameObject bullet;
+    [SerializeField] private float _fireDelay;
+    [SerializeField] private int _maxSpeed;
+    [SerializeField] private GameObject _bullet;
 
-    // Use this for initialization
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
@@ -26,15 +25,15 @@ public class PlayerController : MonoBehaviour
         }
         else if (Input.GetAxis("Fire1") > 0)
         {
-            var newBullet = Instantiate(bullet, transform.position, transform.rotation);
+            var newBullet = Instantiate(_bullet, transform.position, transform.rotation);
             Physics2D.IgnoreCollision(newBullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
-            _nextFire = fireDelay;
+            _nextFire = _fireDelay;
         }
     }
 
     void FixedUpdate()
     {
-        _rigidbody.velocity = maxSpeed * new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        _rigidbody.velocity = _maxSpeed * new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         var position = Camera.main.WorldToScreenPoint(transform.position);
         var mouse = Input.mousePosition;
         var angle = Mathf.Atan2(mouse.y - position.y, mouse.x - position.x) * Mathf.Rad2Deg;
---
+++ Assets/Scripts/SnowmanController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowmanController : MonoBehaviour
+{
+    private GameObject _player;
+    private float _hp = 0;
+    private Rigidbody2D _myRigidbody;
+    
+    [SerializeField] private float _walkSpeed;
+    [SerializeField] private float _maxHP;
+    
+    // Use this for initialization
+    void Start()
+    {
+        _player = GameObject.Find("Player");
+        _hp = _maxHP;
+        _myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var walk = new Vector3(0, _walkSpeed);
+        _myRigidbody.rotation =
+            Mathf.Atan2(transform.position.x - _player.transform.position.x,
+                _player.transform.position.y - transform.position.y) * Mathf.Rad2Deg;
+        GetComponent<Rigidbody2D>().velocity = transform.rotation * walk;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player Bullet"))
+        {
+            if (--_hp <= 0)
+                Destroy(gameObject);
+        }
+    }
+}
